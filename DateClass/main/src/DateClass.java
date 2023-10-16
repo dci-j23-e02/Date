debffc9652518e1bedfb817dcbe9d42f987f3cad
safwan-kher <safwan.kherallah@digitalcareerinstitute.org>
2023-10-16 10:06:10 +0200

    init commit

---
+++ DateClass/main/src/DateClass.java
@@ -0,0 +1,24 @@
+import java.util.Date;
+
+public class DateClass {
+public static void main(String[] args){
+  /*
+  * Variable names rules in Java
+  * can not start with a number
+  * can not contain any special characters But only ('_' and '$')
+  * can contain alphanumeric characters
+  * can start with  ('_' or '$')
+  *
+  * Variable names best practices in Java
+  * use either Camel case (camelCase) or snake case (snake_case)
+  * give a meaningful name
+  * constants are named always with full name capitalised (NUMBERS)
+  * */
+  // Date dates_1$ = new Date();
+  // Date $dates = new Date();
+ // Date _dates = new Date();
+
+
+
+}
+}
